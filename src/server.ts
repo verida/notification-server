50a0ae647ecc174b13218e9cdb27e759bf6d29b4
Chris <chris@verida.io>
2021-12-11 10:57:55 +1030

    Add request validation that requires a valid signature from a Verida DID.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import router from './routes'
 import Db from './db'
+const basicAuth = require('express-basic-auth')
+import RequestValidator from './request-validator'
 
 import dotenv from 'dotenv'
 dotenv.config()
@@ -11,6 +13,11 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(basicAuth({
+  authorizer: RequestValidator.authorize,
+  authorizeAsync: true,
+  unauthorizedResponse: RequestValidator.getUnauthorizedResponse
+}))
 app.use(router)
 
 Db.init()
