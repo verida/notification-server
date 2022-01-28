dbc40fb97cb829a26f717b0f9385d52e3401555a
Nick Lothian <nick@verida.io>
2022-01-28 13:39:22 +1030

    initial experimental commit

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,4 @@ app.use(router)
 
 Db.init()
 
-const PORT = process.env.PORT ? process.env.PORT : 5011;
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
-});
+module.exports=app
